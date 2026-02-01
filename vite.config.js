056aefeae836888ad80ce2991afb9aec7e273e1b
brynne0 <brynne.obrien@me.com>
2026-02-01 13:17:58 +0100

    Add PWA support and install prompt functionality.
    - Added vite-plugin-pwa to enable PWA features.
    - Updated site.webmanifest to include additional icon for PWA.
    - Implemented useInstallPrompt hook to manage installation prompts.
    - Added install prompt button in the User icon

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,45 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Rezepte",
+        short_name: "Rezepte",
+        theme_color: "#f2e7d2",
+        background_color: "#f2e7d2",
+        display: "standalone",
+        start_url: "/",
+        scope: "/",
+        orientation: "portrait-primary",
+        icons: [
+          {
+            src: "/eichhörnchen/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/eichhörnchen/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/eichhörnchen/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
