693e034b9e292f518df9690ba9d680d6fada4566
brynne0 <brynne.obrien@me.com>
2026-09-09 06:46:30 +0200

    Remove all old styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
 import "./lib/i18n";
-import "../src/styles/index.css";
-import "./styles/tailwind.css";
+import "./style.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
