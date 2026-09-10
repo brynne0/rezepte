05340a64a646f9836be600ecb2ecf7804a019cd3
brynne0 <brynne.obrien@me.com>
2026-09-10 17:39:05 +0200

    Fix autofill for sign in and minor layout edit

--- src/App.jsx
+++ src/App.jsx
@@ -194,7 +194,7 @@ function Layout() {
       )}
       <ScrollArea
         className="min-h-0 flex-1"
-        viewportClassName="pb-6 md:pb-8"
+        viewportClassName="pb-8"
         viewportRef={mainScrollRef}
       >
         <div className="mx-auto w-full max-w-7xl px-3 pt-3 md:px-8 md:pt-4">
